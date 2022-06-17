3486bab6fecdf9aa65d4784450d859aaef4d979a
ramnan123 <palyramnan019@gmail.com>
2022-06-17 13:34:31 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 
 <h1>Hello world</h1>
 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim reiciendis consequatur repellat incidunt, corrupti necessitatibus dolore voluptatem ea quo nam sapiente hic vero ducimus error maxime quas dolorum iure nulla?</p>
+<ul>
+    <li>satu</li>
+    <li>dua</li>
+</ul>
     
 </body>
 </html>
